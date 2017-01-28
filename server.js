0127b95c97ce662a7e784963dc16da374114ac08
Michael Saul <michael.saul@gmail.com>
2017-01-27 19:46:19 -0800

    Reverted server port and added engine requirement.

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ var file = new static.Server( './public', {
     gzip: true
 } );
 
-var port = process.env.SERVER_PORT || 8181;
+var port = process.env.PORT || 8181;
 
 // serve
 http.createServer( function ( request, response ) {
@@ -23,5 +23,3 @@ http.createServer( function ( request, response ) {
         file.serve( request, response );
     } ).resume();
 } ).listen( port );
-
-console.log("Server is listening on port " + port);
