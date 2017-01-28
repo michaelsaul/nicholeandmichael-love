4bbc255778f675b0170addfa359514e60ac906ce
Michael Saul <michael.saul@gmail.com>
2017-01-27 18:23:22 -0800

    Added node-static server

---
+++ server.js
@@ -0,0 +1,25 @@
+/**
+ * Static HTTP Server
+ *
+ * Create a static file server instance to serve files
+ * and folder in the './public' folder
+ */
+
+// modules
+var static = require( 'node-static' ),
+    http = require( 'http' );
+
+// config
+var file = new static.Server( './public', {
+    cache: 3600,
+    gzip: true
+} );
+
+var port = normalizePort(process.env.PORT || '8080');
+
+// serve
+http.createServer( function ( request, response ) {
+    request.addListener( 'end', function () {
+        file.serve( request, response );
+    } ).resume();
+} ).listen( port );
