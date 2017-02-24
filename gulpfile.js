262061ae88f44f0d5bda56df0f556f528c659aca
Michael Saul <michael.saul@gmail.com>
2017-02-23 17:25:35 -0800

    Updated plugins and gulpfile.

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,8 @@ var gulp = require('gulp'),
     uglify = require('gulp-uglify'),
     rename = require('gulp-rename'),
     notify = require('gulp-notify'),
-    bower = require('gulp-bower');
+    bower = require('gulp-bower'),
+    merge = require('merge-stream');
 
 var config = {
     sassPath:   './assets/sass',
@@ -13,7 +14,8 @@ var config = {
     jsFile:     '/style-02.js',
     cssOutPath:    './public/css',
     jsOutPath:  './public/js',
-    bowerDir:   './bower_components'
+    bowerDir:   './bower_components',
+    jsPluginsDir:   './public/js/plugins'
 }
 
 gulp.task('bower', function() {
@@ -50,12 +52,23 @@ gulp.task('js', function () {
         .pipe(gulp.dest(config.jsOutPath))
 });
 
-gulp.task('jquery', function () {
-    return gulp.src(config.bowerDir + '/jquery/dist/jquery.min.js')
-    .pipe(gulp.dest('./public/js/plugins'));
-})
+gulp.task('plugins',function () {
+    var bootstrap = gulp.src(config.bowerDir + '/bootstrap-sass/assets/javascripts/bootstrap.min.js')
+    .pipe(gulp.dest(config.jsPluginsDir));
 
-gulp.task('default', ['bower', 'icons', 'css','js', 'jquery'], function() {
+    var jquery = gulp.src(config.bowerDir + '/jquery/dist/jquery.min.js')
+    .pipe(gulp.dest(config.jsPluginsDir));
+
+    var flexslider = gulp.src(config.bowerDir + '/flexslider/jquery.flexslider-min.js')
+    .pipe(gulp.dest(config.jsPluginsDir));
+
+    var scrollreveal = gulp.src(config.bowerDir + '/scrollreveal/dist/scrollreveal.min.js')
+    .pipe(gulp.dest(config.jsPluginsDir));
+
+    return merge(bootstrap, jquery, flexslider, scrollreveal);
+});
+
+gulp.task('default', ['bower', 'icons', 'css','js', 'plugins'], function() {
     gulp.watch(config.sassPath + '/*.scss', ['css']);
     gulp.watch(config.jsPath + '/*.js', ['js']);
 });
